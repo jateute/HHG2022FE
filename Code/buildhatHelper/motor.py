4cd19cef772d09dddf57d99dfbecd0014bc567a2
PascalRombach <pasc.rombach@heinrichheinedo.de>
2022-05-10 15:11:07 +0200

    Implemented automatic positioning and fixed a bug

--- Code/buildhatHelper/motor.py
+++ Code/buildhatHelper/motor.py
@@ -15,17 +15,27 @@ class Driving_Motor(buildhat.Motor):
         return self._cm_per_rotation
 
 class Steering_Motor(buildhat.Motor):
-    __slots__ = ("upper_bound", "lower_bound")
+    __slots__ = ("_upper_bound", "_lower_bound","_target_degrees","_speed")
     def __init__(self, port : str, upper_bound : int, lower_bound : int):
-        super().__init__()
+        super().__init__(port)
 
-        self.upper_bound = upper_bound
-        self.lower_bound = lower_bound
+        self._upper_bound = upper_bound
+        self._lower_bound = lower_bound
         pass
 
     def run_to_position(self, degrees, speed=None, blocking=True):
-        degrees = min(max(degrees,self.lower_bound),self.upper_bound)
+        degrees = min(max(degrees,self._lower_bound),self._upper_bound)
 
         super().run_to_position(degrees,speed,blocking)
+        pass
+
+    
+    def set_target_position(self, degrees, speed=None):
+        self._target_degrees = degrees
+        self._speed = speed
+        pass
+
+    def update(self):
+        self.run_to_position(self._target_degrees,self._speed,False)
         pass
     pass
